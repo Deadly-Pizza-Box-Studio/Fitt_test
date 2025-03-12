be2c68047b0e819430d12be3e9642e5b1c44fe82
Ppet456 <ps.gila456@gmail.com>
2025-03-12 09:43:23 +0100

    js.script uprade

---
+++ script.js
@@ -0,0 +1,25 @@
+let score = 0;
+function submit()
+{
+    for(let i = 0;i < 13;i++)
+        {
+            for(let j = 0;j < 3;i++)
+                {
+                    if(document.getElementById("q"+i+j).checked)
+                        {
+                            score += document.getElementById("q"+i+j).value
+                        }
+                }
+        }
+    const data = {Name:"score",Score: score}
+    fetch("scores.json",
+        {
+            method:"post",
+            headers:{'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+    .then(json=>json.json())
+    .then(result=>console.log(result))
+}
+
+
